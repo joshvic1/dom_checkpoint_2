4a81adab033720de0bc1002f2ca1e3f7588e39e5
[Olatunbosun Victor] <[olatunbosunvictor1@gmail.com]>
2025-04-28 15:57:48 +0100

    Added new changes

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   const colorBox = document.getElementById("color-box");
   const changeColorBtn = document.getElementById("change-color-btn");
+  //   This will get the random color and change it to hexadecimal
   function getRandomColor() {
     const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
     colorBox.style.backgroundColor = randomColor;
   }
+  //   Function to click the button and call the randomcolor function
   changeColorBtn.addEventListener("click", function () {
     getRandomColor();
   });
